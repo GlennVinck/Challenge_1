b50aae22cc1c1e8182531ce1aa29cbe0e099e2da
GlennVinck <vinck_glenn@hotmail.com>
2022-12-10 16:28:24 +0100

    header finished

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
 
 export default function App() {
     return (
         <ScrollView style={styles.container}>
-            <Text style={styles.header1}>Books</Text>
-            <View></View>
+            <View style={styles.head}>
+                <Text style={styles.header1}>Bookshop</Text>
+                <View style={styles.cart}>
+                    <Image
+                        style={styles.cartImage}
+                        source={require("./assets/favicon.png")}
+                    />
+                    <Text style={styles.cartCount}>0</Text>
+                </View>
+            </View>
             <StatusBar style="auto" />
         </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
+    head: {
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: "red",
+    },
+
     header1: {
         fontSize: 32,
         fontWeight: "800",
         margin: 12,
+    },
+
+    cart: {
+        margin: 12,
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+
+    cartImage: {
+        width: 30,
+        height: 30,
+        marginRight: 5,
+    },
+
+    cartCount: {
+        fontSize: 24,
+        fontWeight: "400",
     },
 
     container: {
