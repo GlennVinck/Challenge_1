5c78e30360aacfe43270db8d48ba6e55d2c8327d
GlennVinck <vinck_glenn@hotmail.com>
2022-12-10 16:56:18 +0100

    finish

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import {
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    Image,
+    Pressable,
+} from "react-native";
 
 export default function App() {
     return (
@@ -14,16 +21,88 @@ export default function App() {
                     <Text style={styles.cartCount}>0</Text>
                 </View>
             </View>
+            <View style={styles.productContainer}>
+                <View style={styles.tile}>
+                    <Text style={styles.header2}>A Book Title</Text>
+                    <Text style={styles.writer}>By Some random dude</Text>
+                    <Text style={styles.details}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam
+                    </Text>
+                    <View style={styles.tileFooter}>
+                        <Text>ISBN 978-8-1783-1191-3</Text>
+                        <Pressable style={styles.button}>
+                            <Text style={styles.text}>READ</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.productContainer}>
+                <View style={styles.tile}>
+                    <Text style={styles.header2}>A Book Title</Text>
+                    <Text style={styles.writer}>By Some random dude</Text>
+                    <Text style={styles.details}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam
+                    </Text>
+                    <View style={styles.tileFooter}>
+                        <Text>ISBN 978-8-1783-1191-3</Text>
+                        <Pressable style={styles.button}>
+                            <Text style={styles.text}>READ</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.productContainer}>
+                <View style={styles.tile}>
+                    <Text style={styles.header2}>A Book Title</Text>
+                    <Text style={styles.writer}>By Some random dude</Text>
+                    <Text style={styles.details}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam
+                    </Text>
+                    <View style={styles.tileFooter}>
+                        <Text>ISBN 978-8-1783-1191-3</Text>
+                        <Pressable style={styles.button}>
+                            <Text style={styles.text}>READ</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.productContainer}>
+                <View style={styles.tile}>
+                    <Text style={styles.header2}>A Book Title</Text>
+                    <Text style={styles.writer}>By Some random dude</Text>
+                    <Text style={styles.details}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam
+                    </Text>
+                    <View style={styles.tileFooter}>
+                        <Text>ISBN 978-8-1783-1191-3</Text>
+                        <Pressable style={styles.button}>
+                            <Text style={styles.text}>READ</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
             <StatusBar style="auto" />
         </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        paddingTop: 56,
+        backgroundColor: "#fff",
+    },
+
     head: {
         flex: 1,
         flexDirection: "row",
-        backgroundColor: "red",
     },
 
     header1: {
@@ -51,8 +130,48 @@ const styles = StyleSheet.create({
         fontWeight: "400",
     },
 
-    container: {
-        paddingTop: 56,
-        backgroundColor: "#fff",
+    productContainer: {
+        marginTop: 40,
+    },
+
+    tile: {
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginLeft: 20,
+        marginRight: 20,
+        borderBottomColor: "#B4B8DA",
+        borderBottomWidth: 1,
+        borderStyle: "solid",
+    },
+
+    header2: {
+        fontSize: 24,
+        fontWeight: "400",
+    },
+
+    writer: {
+        fontSize: 12,
+        fontWeight: "200",
+        marginBottom: 10,
+    },
+
+    details: {
+        fontSize: 16,
+        fontWeight: "200",
+        marginBottom: 10,
+    },
+
+    tileFooter: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+
+    button: {
+        padding: 5,
+        borderWidth: 1,
+        borderStyle: "solid",
+        borderRadius: 5,
     },
 });
